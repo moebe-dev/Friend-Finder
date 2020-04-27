298d7510d529c3f8758783b4036d42ea29836baf
Muhammad Ali <qtyeert@yahoo.com>
2020-04-26 19:22:31 -0500

    Added CDN Images &  Main Logic Variable

---
+++ app/data/friends.js
@@ -0,0 +1,45 @@
+var friends = [
+    {
+        name: "Gavin Belson",
+        photo:
+            "https://bit.ly/2VYdAqQ",
+        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+    },
+    {
+        name: "Donald Jared Dunn",
+        photo:
+            "https://bit.ly/2VFVDy5",
+        scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+    },
+    {
+        name: "Bertram Gilfoyle",
+        photo:
+            "https://bit.ly/2KFpj88",
+        scores: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+    },
+    {
+        name: "Dinesh Chugtai",
+        photo:
+            "https://bit.ly/2VFznVl",
+        scores: [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+    },
+    {
+        name: "Jian Yang",
+        photo:
+            "https://bit.ly/3570XOc",
+        scores: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+    },
+    {
+        name: "Big Head",
+        photo:
+            "https://bit.ly/2VGkaDp",
+        scores: [4, 3, 4, 2, 4, 3, 5, 3, 5, 4]
+    },
+    {
+        name: "Erlich Bachman",
+        photo:
+            "https://bit.ly/3cKWrHC",
+        scores: [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+    }
+];
+module.exports = friends;
